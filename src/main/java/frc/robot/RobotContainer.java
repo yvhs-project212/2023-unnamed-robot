e37065a6aafd5039a175e9a95e576cfe6ec8f90a
Leq1L1 <26leli001@mdusd.net>
2023-01-31 22:44:49 -0800

    Added Encoder Reading Method

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,11 @@
 
 package frc.robot;
 
+import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -21,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -6,29 +6,35 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DrivetrainSubsystem extends SubsystemBase {
   /** Creates a new DrivetrainSubsystem. */
 
-  public WPI_TalonFX leftTop;
-  public WPI_TalonFX leftBottom;
-  public WPI_TalonFX rightTop;
-  public WPI_TalonFX rightBottom;
+  public WPI_TalonFX leftTopMotor;
+  public WPI_TalonFX leftBottomMotor;
+  public WPI_TalonFX rightTopMotor;
+  public WPI_TalonFX rightBottomMotor;
 
   public DrivetrainSubsystem() {
 
-    leftTop = new WPI_TalonFX(Constants.DrivetrainConstants.LEFT_TOP_MOTOR);
-    leftBottom = new WPI_TalonFX(Constants.DrivetrainConstants.LEFT_BOTTOM_MOTOR);
-    rightTop = new WPI_TalonFX(Constants.DrivetrainConstants.RIGHT_TOP_MOTOR);
-    rightBottom = new WPI_TalonFX(Constants.DrivetrainConstants.RIGHT_BOTTOM_MOTOR);
-
+    leftTopMotor = new WPI_TalonFX(Constants.DrivetrainConstants.LEFT_TOP_MOTOR);
+    leftBottomMotor = new WPI_TalonFX(Constants.DrivetrainConstants.LEFT_BOTTOM_MOTOR);
+    rightTopMotor = new WPI_TalonFX(Constants.DrivetrainConstants.RIGHT_TOP_MOTOR);
+    rightBottomMotor = new WPI_TalonFX(Constants.DrivetrainConstants.RIGHT_BOTTOM_MOTOR);
 
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    double leftTopMotorPos = leftTopMotor.getSelectedSensorPosition();
+    double leftBottomMotorPos = leftBottomMotor.getSelectedSensorPosition();
+    double rightTopMotorPos = rightTopMotor.getSelectedSensorPosition();
+    double rightBottomMotorPos = rightTopMotor.getSelectedSensorPosition();
+
+    SmartDashboard.putNumber("leftTopEncoder", leftTopMotorPos);
   }
 }
