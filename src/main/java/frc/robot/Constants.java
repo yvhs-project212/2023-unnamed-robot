1fbd21149b2e3cc059e625b4f919aeca67bbf004
Leq1L1 <26leli001@mdusd.net>
2023-02-18 13:57:23 -0800

    Added Claw Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,6 +21,17 @@ public final class Constants {
     public static final int RIGHT_BOTTOM_MOTOR = 3;
   }
 
+  
+  public static class ClawConstants{
+    public static final int LEFT_CLAW_ROLLER_MOTOR = 4;
+    public static final int RIGHT_CLAW_ROLLER_MOTOR = 5;
+
+    public static final int CLAW_DOUBLE_SOLENOID_FORWARD = 2;
+    public static final int CLAW_DOUBLE_SOLENOID_REVERSE = 3;
+
+    public static final int CLAW_LIMIT_SWITCH = 9;
+  }
+
   public static class OperatorConstants {
     public static final int DRIVER_CONTROLLER_PORT = 0;
     public static final int OPERATOR_CONTROLLER_PORT = 1;
