2955b1d676ee8ac913493158472b56887683dffc
unknown <your@example.com>
2023-02-18 14:18:42 -0800

    Other stuff

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class ArmSubsystem extends SubsystemBase {
+  /** Creates a new Arm. */
+
+  public WPI_TalonFX armMotor;
+  public double armMotorPos;
+  public double armDown;
+  public double armUp;
+  
+  
+
+  public ArmSubsystem() {
+    armMotor = new WPI_TalonFX(Constants.ArmConstants.ARM_MOTOR);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    armMotorPos = armMotor.getSelectedSensorPosition();
+  
+  }
+  
+  public void armWithPOV(XboxController controller) {
+    if ((controller.getPOV() == 0)) {               // Up D-Pad makes arm go up
+      armMotor.set(-0.5);
+    } else if (controller.getPOV() == 180) {        // Down D-Pad makes arm go down
+      armMotor.set(0.5); 
+    }
+  }
+
+  public void stopMotors() {
+    armMotor.set(0);
+  }
+}
+
+
