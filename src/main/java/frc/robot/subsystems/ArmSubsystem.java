e4b1b56f04289e3419608b4f382bb7346d8e896a
unknown <your@example.com>
2023-03-01 14:56:39 -0800

    Updated self sustaining Code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -38,14 +38,14 @@ public class ArmSubsystem extends SubsystemBase {
   
   public void armWithPOV(XboxController controller) {
     if ((controller.getPOV() == 0)) {               // Up D-Pad makes arm go up
-      armMotor.set(-0.5);
+      armMotor.set(-0.3);
     } else if (controller.getPOV() == 180) {        // Down D-Pad makes arm go down
-      armMotor.set(0.5); 
+      armMotor.set(0.15); 
     } else {
       armMotor.set(0);
       inPlaceArmMotorPos = armMotorPos;
       if (inPlaceArmMotorPos > armMotorPos) {
-        armMotor.set(-0.5);
+        armMotor.set(-0.3);
       } else {
         armMotor.set(0);
       }
