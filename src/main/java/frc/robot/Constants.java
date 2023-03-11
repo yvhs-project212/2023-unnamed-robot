d1e05fe7148159f5badab4f35508462d9e1f61a5
Leq1L1 <26leli001@mdusd.net>
2023-03-10 16:23:58 -0800

    Winner's Code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,7 +23,7 @@ public final class Constants {
     public static final int RIGHT_BOTTOM_MOTOR = 3;
 
     public static final double DRIVE_FORWARD_SPEED = 0.25;
-    public static final double DRIVE_UP_THE_RAMP_kP = 0.008;
+    public static final double DRIVE_UP_THE_RAMP_kP = 0.01;
     public static final double DRIVE_UP_THE_RAMP_kD = 0.005;
 
     public static final int GEAR_SHIFTER_SOLENOID = 0;
@@ -48,7 +48,7 @@ public final class Constants {
     public static final int LEFT_CLAW_ROLLER_MOTOR = 5;
     public static final int RIGHT_CLAW_ROLLER_MOTOR = 8;
     public static final double CLAW_REMOTE_OUTTAKE_SPEED = 0.5;
-    public static final double CLAW_AUTO_OUTTAKE_SPEED = 0.5;
+    public static final double CLAW_AUTO_OUTTAKE_SPEED = 0.4;
     public static final int AUTO_CLAW_OUTTAKE_TIMING = 8000;
 
     public static final int CLAW_SOLENOID = 1;
